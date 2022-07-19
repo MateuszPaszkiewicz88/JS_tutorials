c1fc6722a21b53048f8d4d87040ab40666425864
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-07-19 10:39:54 +0200

    Add additional questions to quiz

--- JS - quiz/main.js
+++ JS - quiz/main.js
@@ -1,4 +1,20 @@
 alert("WItam w Ninja Quiz!");
-const question = "Jakie ziemskie imię ma Superman?";
-const answer = prompt(question);
-alert(`Twoja odpowiedź to: ${ answer }.`);
+
+const quiz = [
+    ["Jak na imię miał Superman?","Clark Kent"],
+    ["Jak na imie miała Wonder Woman?","Diana Prince"],
+    ["Jak na imię miał Batman?","Bruce Wayne"]
+]
+
+let score = 0;
+
+for (const[question, answear] of quiz){
+    const response = prompt(question);
+    if (response === answear){
+        alert("Prawidłowa odpowiedź!");
+        score++
+    }else{
+        alert(`Niestety, prawidłowa odpowiedź to ${answear}`)
+    }
+}
+alert(`Zdobyłeś łącznie ${score} punktów.`)
