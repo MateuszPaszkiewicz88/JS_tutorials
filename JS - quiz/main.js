703ffc2517c82a461fbf663d4ec8ffef5932b8f8
Mateusz Paszkiewicz <mateusz.paszkiewicz@ergohestia.pl>
2022-07-19 13:32:43 +0200

    change lang to pl

--- JS - quiz/main.js
+++ JS - quiz/main.js
@@ -1,32 +1,40 @@
 alert("WItam w Ninja Quiz!");
-
 const quiz = [
-    ["Jak na imię miał Superman?","Clark Kent"],
-    ["Jak na imie miała Wonder Woman?","Diana Prince"],
-    ["Jak na imię miał Batman?","Bruce Wayne"]
-]
+  { name: "Superman", realName: "Clark Kent" },
+  { name: "Wonder Woman", realName: "Diana Prince" },
+  { name: "Batman", realName: "Bruce Wayne" },
+];
 
-function start(quiz){
-    let score = 0;
-    for (const[question,answer] of quiz){
-        const response = ask(question);
-        check(response,answer);
-    }
-gameOver();
+const game = {
+  start(quiz) {
+    this.questions = [...quiz];
+    this.score = 0;
 
-function ask(question){
-    return prompt(question);
-}
-function check(response,answer){
-    if(response===answer){
-        alert("Prawidłowa odpowiedź!");
-        score++;
-    }else{
-        alert(`Źle. Prawidłowa odpowiedź to ${answer}`)
+    for (const question of this.questions) {
+      this.question = question;
+      this.ask();
     }
-}
-function gameOver(){
-    alert(`Koniec gry. Uzyskany wynik to: ${score}`)
-}
-}
-start(quiz);
+    this.gameOver();
+  },
+
+  ask() {
+    const question = `Jak ma na imię ma ${this.question.name}?`;
+    const response = prompt(question);
+    this.check(response);
+  },
+
+  check(response) {
+    const answer = this.question.realName;
+    if (response === answer) {
+      alert("Prawidłowa odpowiedź!");
+      this.score++;
+    } else {
+      alert(`Źle. Prawidłowa odpowiedź to ${answer}`);
+    }
+  },
+
+  gameOver() {
+    alert(`Koniec gry. Uzyskany wynik to: ${this.score}`);
+  },
+};
+game.start(quiz);
